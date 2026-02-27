52406b6b8963d23fb39e60491bd9225e7b290fa4
Tinnaphat Somsang <tinvv@outlook.co.th>
2026-02-27 23:03:36 +0700

    fix(cmd/music): remove unrelated default songs

--- server/api/music.ts
+++ server/api/music.ts
@@ -6,21 +6,8 @@ export function registerMusicAPI(app: Elysia) {
   app.get("/api/queue", () => songQueue);
 
   app.get("/api/defaultSong", () => {
-    let defaultSong = getDefaultSong();
-
-    if (!defaultSong) {
-      defaultSong = [
-        {
-          title: "Sad Flower",
-          author: "Reinizra",
-          thumbnail:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB4i7JLl4BtWz4gYzUnsx6WcYDAK74ScNGzQ&s",
-          id: "agPF9Eptt1s",
-        },
-      ];
-    }
-
-    return defaultSong;
+    const defaultSong = getDefaultSong();
+    return defaultSong ?? [];
   });
   return app;
 }
